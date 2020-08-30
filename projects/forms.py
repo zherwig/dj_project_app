40fbefde39017f5196afecf5cf487d8b99791d44
Zjef Herwig <zherwig@hotmail.com>
2020-08-30 06:21:41 +0300

    Added all kinds of customization to custom form (classes, labels, placeholders)

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Project
 from django.contrib.auth.models import User
+import datetime
 
 class ProjectCreationForm(forms.ModelForm):
     class Meta:
@@ -9,14 +10,19 @@ class ProjectCreationForm(forms.ModelForm):
 
 
 class RawProjectCreationForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={
+            'placeholder' : "The project Title",
+            'class': "form-control",
+            'id': 'title_form_field'
+    }))
     detail = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20}))
-    closed = forms.BooleanField()
+    closed = forms.BooleanField(label="Completed")
     onHold = forms.BooleanField()
     priority = forms.IntegerField()
     placeInOrder = forms.IntegerField()
     duedate = forms.DateField()
     owner = forms.ModelChoiceField(queryset = User.objects.all())
     assignee = forms.ModelChoiceField(queryset = User.objects.all())
-    created_at = forms.DateTimeField()
-    updated_at = forms.DateTimeField()
+    created_at = forms.DateTimeField(initial=datetime.datetime.now(), widget=forms.HiddenInput())
+    updated_at = forms.DateTimeField(initial=datetime.datetime.now())
