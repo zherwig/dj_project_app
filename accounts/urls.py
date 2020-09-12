2f84b42135aba18118eca4f542163fb9581ea57e
Zjef Herwig <zherwig@hotmail.com>
2020-09-12 14:34:53 +0300

    Cleaned up accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('register/', views.registerPage, name="register_page"),
 	path('login/', views.loginPage, name="login_page"),  
 	path('logout/', views.logoutUser, name="logout_page"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render, redirect
-from accounts.forms import UserForm, ProfileForm
 from django.contrib.auth.decorators import login_required
-from django.db import transaction
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-#from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
-from accounts.models import Profile
-from accounts.forms import CreateUserForm
-
-def registerPage(request):
-	if request.user.is_authenticated:
-		return redirect('dashboards:home')
-	else:
-		form = CreateUserForm()
-		if request.method == 'POST':
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
-
-				return redirect('login_page')
-			
-
-		context = {'form':form}
-		return render(request, 'register.html', context)
 
 def loginPage(request):
 	if request.user.is_authenticated:
@@ -49,26 +25,8 @@ def loginPage(request):
 		context = {}
 		return render(request, 'login.html', context)
 
+@login_required
 def logoutUser(request):
 	logout(request)
 	return redirect('login_page')
 
-@login_required
-@transaction.atomic
-def update_profile(request):
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            return redirect('settings:profile')
-        else:
-            pass
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, './profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
