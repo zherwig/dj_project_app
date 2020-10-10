7f174bbde07ed42b21a6068703757d195fbb9d6e
Zjef Herwig <zherwig@hotmail.com>
2020-10-10 09:30:34 +0300

    * Added tasks to dashboard in page search * Added complete button to project page

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -23,18 +23,31 @@ topicSelect.onchange = function() {
     dashboardOnPageSearchTerm = document.getElementById('dashboardOnPageSearchTerm').value.toLowerCase();
     // Getting topic sections
     topicSections = document.getElementsByClassName("topic_row")
-    // If search term is not empty loop over topics and projects, hide all,
-    // the cards and topics then unhide those that match
+    // If search term is not empty
     if(dashboardOnPageSearchTerm !== ""){
+        //Loop over top sections
         for(var i = 0;i < topicSections.length;i++){
+            // hide all topic rows
             topicSections[i].classList.add("dashboard_hidden_row")
+            //get all cpjects cards per topic, hide them
             projectCards = topicSections[i].getElementsByClassName("dashboardcard")
             for(var j = 0;j < projectCards.length;j++){
                 projectCards[j].parentNode.classList.add("dashboard_hidden_card")
+                // Get inner text and unhide if matches search
                 projectTitle = projectCards[j].getElementsByTagName("h5")[0].innerText.toLowerCase()
                 if(projectTitle.search(dashboardOnPageSearchTerm) !== -1 ){
                     topicSections[i].classList.remove("dashboard_hidden_row")
                     projectCards[j].parentNode.classList.remove("dashboard_hidden_card")
+                } else {
+                    // Otherwise loop over tasks and see if they match
+                    taskTitles = projectCards[j].getElementsByClassName("dashboard_card_task_title_text")
+                    for(var h = 0;h < taskTitles.length;h++){
+                        if(taskTitles[h].innerText.toLowerCase().search(dashboardOnPageSearchTerm) !== -1){
+                            topicSections[i].classList.remove("dashboard_hidden_row")
+                            projectCards[j].parentNode.classList.remove("dashboard_hidden_card")
+                            break
+                        } 
+                    }
                 }
             }
         }
