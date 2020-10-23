71e0c519655a2b411387b59f1ba235f1dab2d87d
Zjef Herwig <zherwig@hotmail.com>
2020-10-23 08:56:50 +0300

    * Better note logging on to dos page * Better note relations * Show where note was created

--- notes/forms.py
+++ notes/forms.py
@@ -4,6 +4,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML, Div, Field, Layout, Submit
 
 class NoteCreationForm(forms.ModelForm):
+    previous_url = forms.URLField(required=False)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from notes.models import Note
 from tasks.models import Task
+from actions.models import Action
+from projects.models import Project
 from notes.forms import NoteCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -54,21 +56,30 @@ def note_delete_view(request, id):
 
 @login_required
 def note_create_view(request, taskid=None, projectid=None, actionid=None):
-    initial_data = {}
+    initial_data = {
+        'previous_url' : request.META.get('HTTP_REFERER')
+    }
     if taskid:
         initial_data['task'] = taskid
+        current_task = Task.objects.filter(id=taskid)
+        initial_data['project'] = current_task.project.id
     elif projectid:
         initial_data['project'] = projectid
     elif actionid:
         initial_data['action'] = actionid
+        current_action = Action.objects.filter(id=actionid)
+        initial_data['task'] = current_action[0].task.id
+        initial_data['project'] = current_action[0].task.project.id
+    print(initial_data)
     form = NoteCreationForm(request.POST or None, initial=initial_data)
     if form.is_valid():
+        previous_ulr = form.cleaned_data['previous_url']
         form_obj = form.save()
         form_obj.updated_at = datetime.datetime.now()
         form_obj.note_updated_by = request.user
         form_obj.note_created_by = request.user
         form_obj.save()
-        form = NoteCreationForm()
+        return redirect(previous_ulr)
     context = {
         "form" : form
     }
